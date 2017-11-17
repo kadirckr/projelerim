214be04137b7258f88dc55ebd5684ba762b4f25a
kadirckr <ckr_kadir@outlook.com>
2017-11-17 16:12:23 +0300

    proje3

---
+++ proje3.Model/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje3.Model
+{
+public    class IUser
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string TC { get; set; }
+    }
+}
